98726d1fb880abbf74371fc0c5cea607a55d56d3
lemoncube1 <lemonglasgroup@protonmail.com>
2021-07-29 23:29:22 +0300

    episode 3 complete

--- tests/TagParserTest.php
+++ tests/TagParserTest.php
@@ -7,21 +7,22 @@ use PHPUnit\Framework\TestCase;
 
 class TagParserTest extends TestCase
 {
+    protected TagParser $parser;
+
     public function test_it_parses_a_single_tag()
     {
-        $parser = new TagParser;
-
-        $result = $parser->parse('personal');
+//        Act
+        $result = $this->parser->parse('personal');
         $expected = ['personal'];
 
+//        Assert
         $this->assertSame($expected, $result);
     }
 
     public function test_it_parses_a_comma_separated_list_of_tags()
     {
-        $parser = new TagParser;
 
-        $result = $parser->parse('personal, money, family');
+        $result = $this->parser->parse('personal, money, family');
         $expected = ['personal', 'money', 'family'];
 
         $this->assertSame($expected, $result);
@@ -29,9 +30,8 @@ class TagParserTest extends TestCase
 
     public function test_it_parses_a_pipe_separated_list_of_tags()
     {
-        $parser = new TagParser;
 
-        $result = $parser->parse('personal | money | family');
+        $result = $this->parser->parse('personal | money | family');
         $expected = ['personal', 'money', 'family'];
 
         $this->assertSame($expected, $result);
@@ -39,14 +39,18 @@ class TagParserTest extends TestCase
 
     public function test_spaces_are_optional()
     {
-        $parser = new TagParser;
 
-        $result = $parser->parse('personal,money,family');
+        $result = $this->parser->parse('personal,money,family');
         $expected = ['personal', 'money', 'family'];
         $this->assertSame($expected, $result);
 
-        $result = $parser->parse('personal|money|family');
+        $result = $this->parser->parse('personal|money|family');
         $expected = ['personal', 'money', 'family'];
         $this->assertSame($expected, $result);
+    }
+
+    protected function setUp(): void
+    {
+        $this->parser = new TagParser();
     }
 }
